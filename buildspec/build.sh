5965f447c6d152f9a7466f463f6b7f3121b7c8ea
jerome@home <github@jeromedecoster.com>
2020-04-21 16:44:13 +0200

    :boom:

--- buildspec/build.sh
+++ buildspec/build.sh
@@ -16,6 +16,44 @@ terraform init \
     -backend-config="key=terraform" \
     -no-color
 
+NAME=$(terraform output | grep ^project_name | sed 's|.*= ||')
+echo ······ NAME=$NAME ······
+
+if [[ -n "$NAME" ]]; then
+    ID=$(aws autoscaling describe-auto-scaling-groups \
+        --auto-scaling-group-names $NAME \
+        --query "AutoScalingGroups[?AutoScalingGroupName == '$NAME'].Instances[*].[InstanceId]" \
+        --output text)
+    echo ······ ID=$ID ······
+
+    if [[ -n "$ID" ]]; then
+        echo ······ terminate EC2 instances ······
+        echo "$ID" | while read line; do
+            aws ec2 terminate-instances --instance-ids $line
+        done
+        echo ······ sleep 5 seconds ······
+        sleep 5
+    fi
+    echo ······ delete auto scaling group ······
+    aws autoscaling delete-auto-scaling-group \
+        --auto-scaling-group-name $NAME \
+        --force-delete
+    echo ······ sleep 10 seconds ······
+    sleep 10
+
+    while [[ -n $(aws autoscaling describe-auto-scaling-groups \
+        --auto-scaling-group-names $NAME \
+        --query "AutoScalingGroups[?AutoScalingGroupName == '$NAME']" \
+        --output text) ]]; do
+        aws autoscaling describe-auto-scaling-groups \
+            --auto-scaling-group-names $NAME \
+            --query "AutoScalingGroups[?AutoScalingGroupName == '$NAME'].[Status]" \
+            --output text
+        echo ······ waiting auto-scaling-group destruction. sleep 20 seconds ······
+        sleep 20
+    done
+fi
+
 echo ······ terraform plan ······
 terraform plan \
     -var "ssh_key_name=$SSH_KEY" \
