9575598b0f18f989fee593774b79d072f21467d6
jerome@home <github@jeromedecoster.com>
2020-04-21 15:59:55 +0200

    :boom:

--- buildspec/pre-build.sh
+++ buildspec/pre-build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-printenv
+echo ·· install terraform ··
 cd /usr/bin
 curl -s -qL -o terraform.zip https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
 unzip -o terraform.zip
-cd $CODEBUILD_SRC_DIR
-cd infra
-terraform fmt -recursive
-terraform validate
+cd $CODEBUILD_SRC_DIR/infra
+echo ·· terraform validate ··
+terraform validate -no-color
